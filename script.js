ff735fccee9295914decdb47d3892abd2c62429c
ShreyVijay <shreyvijay2004@gmail.com>
2025-04-10 14:06:27 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const roles = [
+    "AI & Machine Learning Student",
+    "Problem Solver",
+    "Web Developer"
+];
+
+let roleIndex = 0;
+const typingElement = document.getElementById("typing-role");
+
+function changeRole() {
+    typingElement.textContent = roles[roleIndex];
+    roleIndex = (roleIndex + 1) % roles.length;
+}
+
+// Initial call
+setInterval(changeRole, 2500); // change every 2.5 seconds
